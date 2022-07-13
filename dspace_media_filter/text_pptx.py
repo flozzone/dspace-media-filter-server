d17db3a34e667a482c8a950467b888a7852630ca
Florin Hillebrand <florin.hillebrand@tuwien.ac.at>
2022-07-13 11:24:47 +0200

    Implement exception handling for PPTX extraction errors.

--- dspace_media_filter/text_pptx.py
+++ dspace_media_filter/text_pptx.py
@@ -1,10 +1,20 @@
-from dspace_media_filter.filter import MediaFilterRequest
+import pptx.exc
+
+from dspace_media_filter.filter import MediaFilterRequest, MediaFilterResponse
 from pptx import Presentation
 
 from dspace_media_filter.text import TextFilter
 
 
 class PPTTextFilter(TextFilter):
+    def filter(self, req: MediaFilterRequest) -> MediaFilterResponse:
+        try:
+            return super(PPTTextFilter, self).filter(req)
+        except pptx.exc.PackageNotFoundError as e:
+            return MediaFilterResponse(error=f"PPTX extraction could not detect the file as a valid PPTX file")
+        except pptx.exc.PythonPptxError as e:
+            return MediaFilterResponse(error=f"PPTX extraction failed with error {e.__class__.__name__}")
+
     def filter_text(self, req: MediaFilterRequest) -> str:
         # the following could be also rewritten as a "one"-liner
         # return '\n'.join(['\n'.join([shape.text for shape in slide.shapes if hasattr(shape, "text")])
@@ -12,6 +22,7 @@ class PPTTextFilter(TextFilter):
 
         text = ""
         prs = Presentation(req.abs_file)
+
         for slide in prs.slides:
             for shape in slide.shapes:
                 if hasattr(shape, "text"):
